52ee3b1cf89357a71d86326349eb11d25a9761a9
diana <dinamenediana@gmail.com>
2021-03-10 13:03:58 +0800

    update

---
+++ Minimo Blog/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Optimize the size of css file
+  // purge: {
+  //   enabled: true,
+  //   content: ["./src/*.html"],
+  // },
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
